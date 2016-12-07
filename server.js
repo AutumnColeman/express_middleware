e6fa81ea6c319f969c4bdfa36ed76a6bc03fc6b8
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-07 14:41:35 -0500

    Finished

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const marked = require('marked');
 const morgan = require('morgan');
+const randomString = require('randomstring');
+const uuid = require('uuid');
 const app = express();
+const tokens = {
+  "opensesame" : "toby",
+  "Te2xlO9bzUhmxe7TG6JftrFVMBxsJtXn" : "autumn",
+};
 
 // create a write stream (in append mode)
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
@@ -11,7 +17,7 @@ var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a
 app.use(bodyParser.json());
 
 // setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
+app.use(morgan('common', {stream: accessLogStream}));
 
 app.set('view engine', 'hbs');
 
@@ -19,6 +25,39 @@ app.use(function middle(request, response, next) {
   console.log('Request method:', request.method, 'Request path:', request.path);
   next();
 });
+
+function auth(request, response, next) {
+    console.log(tokens);
+    //verfiy auth token
+    if (request.query.token in tokens) {
+      // console.log(request.query.token);
+      next();
+    } else {
+      response.status(401);
+      response.json({ error: 'You are not logged in' });
+    }
+}
+
+app.post('/api/login', function(request, response) {
+  var username = request.body.username;
+  var password = request.body.password;
+
+  if (username === "toby" && password === "opensesame" ||
+      username === "autumn" && password === "1234") {
+    var token = randomString.generate();
+    tokens[token] = username;
+    response.json({
+      username: username,
+      token: token
+    });
+  } else {
+    response.status(400);
+    response.json({error: 'Login failed' });
+  }
+
+});
+
+app.use(auth);
 
 app.put('/documents/:filepath', function(request, response) {
   var filepath = request.params.filepath;
@@ -87,5 +126,5 @@ app.delete('/documents/:filepath', function(request, response) {
 });
 
 app.listen(3000, function() {
-  console.log('I am listening on 3000.');
+  console.log('Listening on *3000.');
 });
